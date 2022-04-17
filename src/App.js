e9e5484cfbf7554bc401747f19a94c33c5416d43
nishattasnim08 <nishattasnim24258@gmail.com>
2022-04-18 01:12:03 +0600

    banner added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import NotFound from './Components/NotFound/NotFound';
-import Header from './Components/Header/Header';
+import Header from './Pages/Common/Header/Header';
+import NotFound from './Pages/NotFound/NotFound';
+import Home from './Pages/Home/Home/Home';
+
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="*" element={<NotFound></NotFound>} />
       </Routes>
     </div>
