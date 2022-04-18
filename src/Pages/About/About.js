557d3fc69cc5ed18db9b97eeaef76044d36aa618
nishattasnim08 <nishattasnim24258@gmail.com>
2022-04-18 20:26:00 +0600

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import nishat from '../../images/about/nishat.png'
 
 const About = () => {
     return (
@@ -10,11 +11,12 @@ const About = () => {
             <div>
                 <Container>
                     <Row>
-                        <Col>
-                        <h1>Nishat Tasnim</h1>
-                        <p></p>
+                        <Col className='text-start p-5'>
+                            <h1>Nishat Tasnim</h1>
+                            <p>As a potential junior Web developer, I want to be able to create Web sites that successfully present information, enable interaction, and are visually appealing to users. To do so, I'll need to be familiar with the intricacies of Web design and delivery, as well as the many tools and software programs available.<br />
+                                I need to be able to put the information together in a way that my audience can understand.</p>
                         </Col>
-                        <Col>2 of 2</Col>
+                        <Col><img src={nishat} alt=""  className='w-50 m-3' /></Col>
                     </Row>
                 </Container>
             </div>
